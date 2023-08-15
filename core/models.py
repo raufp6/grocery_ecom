8f2dec4c3c83d59b6464cef4f1e8b01362324108
Abdul Rauf <raufp6@gmail.com>
2023-08-15 21:33:30 +0530

    tag removed from product

--- core/models.py
+++ core/models.py
@@ -143,7 +143,6 @@ class Product(models.Model):
     pid = ShortUUIDField(unique=True, length=10, max_length=20)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True,related_name="category")
-    # tags = models.ForeignKey(Tags, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=100, default=None)
     image = models.ImageField(upload_to="products", default='product-icon.png')
     # description = models.TextField(null=True, default=None,blank = True)
