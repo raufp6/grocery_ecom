ac3d83236475d0e84c0d8d69d5ef41a48bb53952
Abdul Rauf <raufp6@gmail.com>
2023-08-15 15:27:57 +0530

    statc file name updated

--- grocery_ecom/settings.py
+++ grocery_ecom/settings.py
@@ -150,7 +150,7 @@ CART_SESSION_ID = 'cart'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
